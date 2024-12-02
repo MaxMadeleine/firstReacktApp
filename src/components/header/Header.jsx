375d03a1535b5481854feac186c67a5ee86d26e3
MaxMadeleine <Max.Madeleine.G@gmail.com>
2024-12-02 10:55:11 +0100

    made navbar component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,12 @@
+import { Navbar } from "../Navbar/Navbar";
+
 export const Header = () => {
-return (
-    <header>
+  return (
+    <>
+      <header>
         <h1>My React App</h1>
-    </header>
-)
+      </header>
+      {<Navbar />}
+    </>
+  );
 };
