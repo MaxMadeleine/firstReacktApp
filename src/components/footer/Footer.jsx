7a8138e8a54b8353c0fc756f22ce3238e41b7f5e
MaxMadeleine <Max.Madeleine.G@gmail.com>
2024-12-02 10:43:18 +0100

    made componets :D

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+export const Footer = () => {
+    return(
+        <footer>
+            <ul>
+            <li><a href="#">Policy</a></li>
+            <li><a href="#">Terms of service</a></li>
+            <li><a href="#">Cookies</a></li>
+            </ul>
+        </footer>
+    )
+}
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,9 @@
+export  const Main = () => {
+    return (
+      <main>
+        <section>
+            <h4>This is main section</h4>
+        </section>
+      </main>
+    )
+}
